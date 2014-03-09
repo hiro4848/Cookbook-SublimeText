1bbd099514f260a51c693a290ddf03938817abc2
unknown <takehiro.takahashi@gmail.com>
2014-03-09 15:50:03 -0700

    1.1.1

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name             'SublimeText'
+maintainer       'my name'
+maintainer_email 'email@example.com'
+license          'All rights reserved'
+description      'description text'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.1.1'
+
+supports          "windows"
+
+
+depends          "windows"
+
+
